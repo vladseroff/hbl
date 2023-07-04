a087ea25c27c9554439106c8d3cfc2386491aa97
vladislav serov <vladseroff@gmail.com>
2023-07-04 19:32:22 +0300

    init 2

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    devtools: {
+        enabled: true
+    },
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@use "@/assets/styles/_colors.scss" as *;'
+                }
+            }
+        }
+    }
+})
